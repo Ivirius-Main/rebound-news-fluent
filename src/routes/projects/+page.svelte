e159df5966430163adf8300228f545342db227b4
jpb <jakub.baranski.33@gmail.com>
2024-05-28 18:53:56 +0100

    Updates to the Projects page

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,1 +1,12 @@
-<h1>In development, coming soon...</h1>
+<section class="">
+    <h1>Rebound Projects</h1>
+    <p>Discover all the projects by the Rebound Team on this page!</p>
+</section>
+<style>
+ .centered-section
+  {
+    text-align: center;
+    margin: 0 auto;
+    padding: 25px;
+    max-width: 1250px;
+  }</style>
