620cd0cb18e100fa1fc13d57a7231d0f2460e4e7
jpb <jakub.baranski.33@gmail.com>
2024-05-28 19:41:59 +0100

    Add some more projects

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -5,13 +5,23 @@
 	let { github, youtube } = links;
 </script>
 <section class="centered-section">
-    <TextBlock variant="titleLarge">Rebound Projects</TextBlock>
+    <TextBlock variant="titleLarge">Rebound Projects</TextBlock><br>
     <TextBlock>Discover all the projects by the Rebound Team on this page!</TextBlock>
 </section>
 <section class="project">
     <TextBlock variant="title">Ivirius Text Editor Plus</TextBlock><br>
     <TextBlock>A versatile, modern, Fluent text editor. Available for $1.09 off the MS Store.</TextBlock><br>
     <TextBlock><b>Developed by:</b> Ivirius, ErrorTek, TheDevil</TextBlock>
+</section>
+<section class="project">
+    <TextBlock variant="title">Ivirius Text Editor</TextBlock><br>
+    <TextBlock>A versatile, modern, Fluent text editor. Available for free off the MS Store and open soruce on GitHub. The latest releases are released on GitHub!</TextBlock><br>
+    <TextBlock><b>Developed by:</b> Ivirius, ErrorTek</TextBlock>
+</section>
+<section class="project">
+    <TextBlock variant="title">UltraTextEdit UWP Plus</TextBlock><br>
+    <TextBlock>Modern, Fluent, Fast</TextBlock><br>
+    <TextBlock><b>Developed by:</b> ErrorTek, Ivirius</TextBlock>
 </section>
 <style>
  .centered-section
@@ -20,5 +30,8 @@
     margin: 0 auto;
     padding: 50px;
     max-width: 1250px;
+  }
+  .project {
+ padding: 15px;
   }
   </style>
